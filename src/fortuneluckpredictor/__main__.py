a2551bde8d304dffcb133fcbddfcbc2e1fcfd268
SamRawdon <sr6360@nyu.edu>
2025-10-30 14:49:38 -0400

    implemented lucky number

--- src/fortuneluckpredictor/__main__.py
+++ src/fortuneluckpredictor/__main__.py
@@ -5,7 +5,7 @@ Entry point for the fortuneluckpredictor package when run as a script.
 from .fortune import fortune
 from .futurePredictions import futurePrediction
 from .compatibility_score import compatibility_score
-
+from .luckyNumber import get_lucky_number
 
 def main() -> None:
     """
@@ -14,6 +14,7 @@ def main() -> None:
     print(fortune()) 
     print(futurePrediction("Alice"))
     print(compatibility_score("Ezio", "Altair"))
+    print(get_lucky_number())
 
 
 if __name__ == "__main__":
